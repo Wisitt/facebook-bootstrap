3daf4fc4bc25372251345e9b329e35bbd9faf6b5
Wisitt <wisit-m@rmutp.ac.th>
2023-08-03 07:23:29 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'facebook-bootstrap',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
